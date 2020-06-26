cd3fecc2971fa8e6bc0577552f967322f225c44e
MertPismisoglu <mpismisoglu@gmail.com>
2020-06-26 20:03:30 +0300

    Initial Commit

---
+++ Flappy CoronaTests/Flappy_CoronaTests.swift
@@ -0,0 +1,34 @@
+//
+//  Flappy_CoronaTests.swift
+//  Flappy CoronaTests
+//
+//  Created by Mert Pişmişoğlu on 26.06.2020.
+//  Copyright © 2020 Mert Pişmişoğlu. All rights reserved.
+//
+
+import XCTest
+@testable import Flappy_Corona
+
+class Flappy_CoronaTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
